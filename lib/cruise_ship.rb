552e1d6b9dd41df1ff15bbb621d97010db47458b
Katrina Brinson <kbrinso@gmail.com>
2018-01-27 01:07:12 +0000

    automatically backed up by learn

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -7,8 +7,11 @@
 # }
 
 def select_winner(passengers)
+  winner = ""
   passengers.each{|suite, name|
-    if suite == :suite_a && name.start_with?
-    
+    if suite == :suite_a && name.start_with?("A")
+      winner = name
+    end
   }
+  winner
 end
